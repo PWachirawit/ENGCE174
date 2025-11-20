080dff490386e41bf490420154b2e6db7579738c
PeachZylnwzaa <Jarungege@gmail.com>
2025-11-20 16:36:24 +0700

    PeachZyNlnwzaa

---
+++ Lab1/Lab1_4.java
@@ -0,0 +1,28 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Lab1_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter score (0-100): ");
+        int score = scanner.nextInt();
+
+        if (score >= 80 && score <= 100) {
+            System.out.println("Grade: A");
+        } else if (score >= 70) {
+            System.out.println("Grade: B");
+        } else if (score >= 60) {
+            System.out.println("Grade: C");
+        } else if (score >= 50) {
+            System.out.println("Grade: D");
+        } else if (score >= 0) {
+            System.out.println("Grade: F");
+        } else {
+            System.out.println("Invalid score!");
+        }
+
+        scanner.close();
+    }
+}
