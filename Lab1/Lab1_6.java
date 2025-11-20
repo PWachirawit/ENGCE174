6ec6478a62752f2c9c43bee189c35b4cc0b2854e
PeachZylnwzaa <Jarungege@gmail.com>
2025-11-20 17:26:09 +0700

    PeachZyNlnwzaa

---
+++ Lab1/Lab1_6.java
@@ -0,0 +1,20 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Lab1_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter multiplication number: ");
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= 12; i++) {
+            int result = number * i;
+            System.out.println(number + " x " + i + " = " + result);
+        }
+
+        scanner.close();
+
+    }
+}
