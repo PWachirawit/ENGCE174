f31c453c1f3d6e1cf81084959de45b232078b8e9
PeachZylnwzaa <Jarungege@gmail.com>
2025-11-21 10:26:46 +0700

    PeachZyNlnwzaa

--- Lab1/Lab1_6.java
+++ Lab1/Lab1_6.java
@@ -6,15 +6,14 @@ public class Lab1_6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter multiplication number: ");
+        System.out.print("Enter multiplication number: ");
         int number = scanner.nextInt();
 
         for (int i = 1; i <= 12; i++) {
             int result = number * i;
-            System.out.println(number + " x " + i + " = " + result);
+            System.out.printf("%d x %d = %d\n", number, i, result);
         }
 
         scanner.close();
-
     }
 }
